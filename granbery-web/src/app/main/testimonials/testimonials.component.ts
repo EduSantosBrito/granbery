9a80e7f87559933735787d2d26e306ef05c7d8ed
edusantosbrito <edu.santos.brito@gmail.com>
2018-06-26 14:25:12 -0300

    Testimonials e deleção de bug do Serviços

---
+++ granbery-web/src/app/main/testimonials/testimonials.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "testimonials",
+    templateUrl: "testimonials.component.html",
+    styleUrls: ['testimonials.component.css']
+})
+
+export class TestimonialsComponent {
+
+}
--- granbery-web/src/app/modules/main/main.module.ts
+++ granbery-web/src/app/modules/main/main.module.ts
@@ -2,19 +2,15 @@ import { AwardsComponent } from './../../main/awards/awards.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from '../../main/main.component';
-import { MenuComponent } from '../../shared/menu/menu.component';
-import { HamburgerIconComponent } from '../../shared/menu/hamburger-icon/hamburger-icon.component';
 import { MenuModule } from '../shared/menu.module';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SwiperModule } from 'ngx-swiper-wrapper';
 import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
 import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 import { ServiceComponent } from '../../main/services/services.component';
+import { TestimonialsComponent } from '../../main/testimonials/testimonials.component';
 
 const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
-  parallax: true,
   direction: 'horizontal',
   pagination: {
     el: '.swiper-pagination',
@@ -38,7 +34,8 @@ const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
   declarations: [
     MainComponent,
     AwardsComponent,
-    ServiceComponent
+    ServiceComponent,
+    TestimonialsComponent,
   ],
   providers: [
     {
